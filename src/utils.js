aed6b4f82bfe3d8d1872c94858e0beab7e2040e6
coha45 <coha464646@gmail.com>
2026-02-15 10:56:16 +0800

    Added truncate

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,8 @@ export const isEmpty = (arr) => {
 export const genId = () => {
     return Math.random().toString(36).substring(2)
 }
+
+export const truncate = (str, max = 10) => {
+    if (str.length >= max) return str.slice(0, max).concat("...")
+    return str
+}
