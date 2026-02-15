98e682c8b4131fc18a002d7483e04dfd814e73b4
coha45 <coha464646@gmail.com>
2026-02-15 10:58:38 +0800

    Add world deletion

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -3,13 +3,13 @@ import Header from '../ui/Header'
 import WorldsContainer from '../ui/WorldsContainer';
 import BtnContainer from '../ui/BtnContainer';
 
-const Main = ({ worlds, addWorld }) => {
+const Main = ({ worlds, delWorld }) => {
   return (
     <div className="h-screen max-h-[600px] max-w-[650px] min-w-[650px] p-10 flex flex-col justify-center gap-2 items-center">
         <Header />
         <div className="w-full flex flex-col items-center justify-center gap-2">  
-          <WorldsContainer worlds={worlds} />
-          <BtnContainer onAdd={addWorld} />
+          <WorldsContainer worlds={worlds} delWorld={delWorld} />
+          <BtnContainer />
         </div>
     </div>
   )
