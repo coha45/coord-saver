12b004f073d836d13577c49ce4f669fdd6cba64e
coha45 <coha464646@gmail.com>
2026-02-15 10:58:02 +0800

    Added react-hook-form for form state management

--- src/components/pages/New.jsx
+++ src/components/pages/New.jsx
@@ -1,45 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from '../ui/Button'
 import { useNavigate } from 'react-router-dom';
 import { versions } from '../../data';
+import { useForm } from 'react-hook-form';
 
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { TiWorld } from "react-icons/ti";
 import { MdEdit } from 'react-icons/md';
+import BackBtn from '../ui/BackBtn';
 
 
 
-const New = () => {
+const New = ({ addWorld }) => {
   const nav = useNavigate()
+  const [file, setFile] = useState("")
+  const  { register, handleSubmit, watch, formState : { errors } } = useForm()
 
-  const handleClick = () => {
+  function handleCreate(data) {
+    const { name, mode, version, modded } = data
+    addWorld(name, mode, version, modded)
     nav("/")
   }
 
   return (
     <>
-      <button className="bg-[#272422] p-2 m-2 text-white absolute left-0 top-0" onClick={handleClick}>
-        <IoMdArrowRoundBack />        
-      </button>
+      <BackBtn />
       <div className="h-screen max-h-[600px] max-w-[650px] min-w-[650px] p-10 flex flex-col justify-center gap-2 items-center">
-        <h2 className="font-xl">Create New World</h2>
-        <form onSubmit={e => e.preventDefault()} className="flex flex-col items-center justify-center gap-2">
-          <input type="text" placeholder="World Name" className="p-2 w-full border-2 border-black text-neutral-600" />
+        <h2 className="text-4xl mb-6 font-mcTen">Create New World</h2>
+        <form onSubmit={handleSubmit(handleCreate)} className="flex flex-col items-center justify-center gap-2">
+          <div className="flex">
+            <label className="w-16 aspect-square bg-neutral-300">
+              <input onChange={(e) => handleUpload(e)} type="file" className="hidden" accept="image/png, image/jpeg" />
+            </label>
+            <input { ...register("name", { required : "Name must not be empty", maxLength : 32 }) } type="text" placeholder="World Name" className="p-2 w-full border-2 border-black text-neutral-600" />
+          </div>
           <div className="w-full flex justify-between items-center gap-2">
             <label className="w-full flex flex-col">
               <h3 className="font-bold">Gamemode</h3>
-              <select className="border-2 border-black w-full">
+              <select { ...register("mode") }  className="border-2 border-black w-full">
                 <option value="survival">Survival</option>
                 <option value="creative">Creative</option>
                 <option value="adventure">Adventure</option>
+                <option value="hardcore">Hardcore</option>
               </select>
             </label>
             <label className="w-full flex flex-col">
               <h3 className="font-bold">Version</h3>
-              <select className="border-2 border-black w-full">
+              <select { ...register("version") } className="border-2 border-black w-full">
                 {
                   versions.map((version) => (
-                    <option value={version}>
+                    <option value={version} key={version}>
                       { version }
                     </option>
                   ))
@@ -47,8 +57,8 @@ const New = () => {
               </select>
             </label>
           </div>
-          <label className="select-none">
-            <input type="checkbox" />
+          <label className="select-none text-xl self-start">
+            <input { ...register("modded") } type="checkbox" />
             Modded
           </label>
           <div className="flex gap-2">
