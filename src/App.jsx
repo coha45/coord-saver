85283c9961bf149cee87a269c5489e8f763b4f46
coha45 <coha464646@gmail.com>
2026-02-15 10:59:20 +0800

    Added Additional functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { genId } from './utils'
 import Main from './components/pages/Main'
 import New from './components/pages/New'
+import Edit from './components/pages/Edit'
 
 const App = () => {
   const [worlds, setWorlds] = useState([])
-  
-  function addWorld(name, mode, version, icon, modded = true,) {
-    if (!name || !mode || !version || !icon) return
+  const nav = useNavigate()
+
+  function addWorld(name, mode, version, modded, icon = "/assets/icn.jpg") {
+    if (!name || !mode || !version || !icon) {
+      console.log("hi")
+      return
+    }
     setWorlds(prevWorlds => [
       {
         id : genId(),
@@ -22,11 +27,26 @@ const App = () => {
     ])
   }
 
+  function delWorld(id) {
+    setWorlds(prevWorlds => prevWorlds.filter(world => world.id !== id))
+  }
+
+  useEffect(() => {
+    const listener = document.addEventListener("keydown", e => {
+      if (e.key === "Escape") {
+        nav("/")
+      }
+    })
+
+    return () => removeEventListener("keypress", listener)
+  }, [])
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Main worlds={worlds} />} />
+        <Route path="/" element={<Main worlds={worlds} delWorld={delWorld} />} />
         <Route path="/new" element={<New addWorld={addWorld} />} />
+        <Route path="/edit/:id" element={<Edit />} />
       </Routes>
     </>
   )
