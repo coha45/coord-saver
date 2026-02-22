42a6051d72c6e714b3d7e9c5cc482875e3285647
coha45 <coha464646@gmail.com>
2026-02-22 14:46:05 +0800

    Added Coordinate class

--- src/classes/Coordinate.js
+++ src/classes/Coordinate.js
@@ -1,5 +1,18 @@
+import { genId } from "../utils"
+
 class Coordinate {
-    constructor() {
-        
+    constructor(name, x, y, z, biome) {
+        this.id = genId()
+        this.name = name
+        this.x = x
+        this.y = y
+        this.z = z
+        this.biome = biome
+    }
+
+    getDistance(coord2) {
+        const { x, y, z } = coord2
     }
 }
+
+export default Coordinate
