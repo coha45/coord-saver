ade54628069c1b905790a328eef28d714468595d
coha45 <coha464646@gmail.com>
2026-02-22 15:19:04 +0800

    Initial UI for edit page

--- src/components/pages/Edit.jsx
+++ src/components/pages/Edit.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 
 import BackBtn from '../ui/BackBtn'
+import Button from '../ui/Button'
 import { useParams } from 'react-router-dom'
 import { versions } from '../../data'
 import { useForm } from 'react-hook-form'
+import CoordinatesContainer from '../ui/CoordinatesContainer'
+import { IoMdAdd } from 'react-icons/io'
+import Coordinate from '../../classes/Coordinate'
 
 const Edit = ({ getWorld }) => {
   const { id } = useParams()
   const [curWorld, setCurWorld] = useState(null)
+  const [coordinates, setCoordinates] = useState([])
   const  { register, handleSubmit, reset, formState : { errors } } = useForm()
 
   useEffect(() => {
@@ -21,45 +26,53 @@ const Edit = ({ getWorld }) => {
     setCurWorld(world)
   }, [curWorld])
 
+  function handleAdd() {
+    const coordObj = new Coordinate("Idk", 123, 304, -32, "Biome")
+    setCoordinates(prevCoords => [...prevCoords, coordObj])
+  }
+
   return (
     <>
         <BackBtn />
-        <div className="h-screen max-h-[600px] max-w-[650px] min-w-[650px] p-10 flex flex-col justify-center gap-2 items-center">
-          <h2 className="text-4xl mb-6 font-mcTen">Create New World</h2>
-          <div className="">
-            <form className="flex flex-col items-center justify-center gap-2">
-              <div className="flex">
-                <input type="text" { ...register("name", { required : "Name must not be empty", maxLength : 32 }) } placeholder="World Name" className="p-2 w-full border-2 border-black text-neutral-600" />
-              </div>
-              <div className="w-full flex justify-between items-center gap-2">
-                <label className="w-full flex flex-col">
-                  <h3 className="font-bold">Gamemode</h3>
-                  <select { ...register("mode") } className="border-2 border-black w-full">
-                    <option value="survival">Survival</option>
-                    <option value="creative">Creative</option>
-                    <option value="adventure">Adventure</option>
-                    <option value="hardcore">Hardcore</option>
-                  </select>
-                </label>
-                <label className="w-full flex flex-col">
-                  <h3 className="font-bold">Version</h3>
-                  <select { ...register("version") } className="border-2 border-black w-full">
-                    {
-                      versions.map((version) => (
-                        <option value={version} key={version}>
-                          { version }
-                        </option>
-                      ))
-                    }
-                  </select>
-                </label>
-              </div>
+        <div className="h-screen max-h-[600px] max-w-[750px] min-w-[650px] p-10 flex flex-col justify-center gap-2 items-center">
+          <h2 className="text-4xl mb-6 font-mcTen">Edit Coordinates</h2>
+          <div className="flex items-center justify-center gap-4">
+            <form className="flex flex-col items-center justify-center gap-2 self-start">
+              <input type="text" { ...register("name", { required : "Name must not be empty", maxLength : 32 }) } placeholder="World Name" className="p-2 w-full border-2 border-black text-neutral-600" />
+              <label className="w-full flex flex-col">
+                <h3 className="font-bold">Gamemode</h3>
+                <select { ...register("mode") } className="border-2 border-black w-full">
+                  <option value="survival">Survival</option>
+                  <option value="creative">Creative</option>
+                  <option value="adventure">Adventure</option>
+                  <option value="hardcore">Hardcore</option>
+                </select>
+              </label>
+              <label className="w-full flex flex-col">
+                <h3 className="font-bold">Version</h3>
+                <select { ...register("version") } className="border-2 border-black w-full">
+                  {
+                    versions.map((version) => (
+                      <option value={version} key={version}>
+                        { version }
+                      </option>
+                    ))
+                  }
+                </select>
+              </label>
               <label className="select-none text-xl self-start">
                 <input { ...register("modded") } type="checkbox" />
                 Modded
               </label>
             </form>
+            <CoordinatesContainer coordinates={coordinates} />
           </div>
+          <span className="self-end">
+            <Button variant="success" onClick={handleAdd}>
+              <IoMdAdd />
+              Add
+            </Button>
+          </span>
         </div>
     </>
   )
